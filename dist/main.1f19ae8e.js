3269e362731a711aff1d61949e5d6f2323f1551e
Lemon <516330797@qq.com>
2020-10-27 17:19:20 +0800

    完成眼睛、鼻子和上嘴唇

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6962" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4035" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
